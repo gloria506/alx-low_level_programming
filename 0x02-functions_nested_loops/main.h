7b236a5dfc3bca0c675cc9e1dd1aa1877cc71867
gloria506 <gloriaboateng2011@gmail.com>
2023-02-21 09:40:25 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 #include <stdio.h>
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,6 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_times_table(int n);
 
 #endif
