6f7bd70df73a2228d278828ce8bfbfec22f9cb2c
gloria506 <gloriaboateng2011@gmail.com>
2023-04-20 08:26:48 -0700

    4puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
