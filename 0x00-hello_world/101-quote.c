71ac2a48d1e1501b51b1b7e93b1d2dc5e35fbccc
gloria506 <gloriaboateng2011@gmail.com>
2023-04-20 08:58:02 -0700

    error task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - the entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
