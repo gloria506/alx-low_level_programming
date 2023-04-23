acbf9b0919c5134514f373492d515459629e03ee
gloria506 <gloriaboateng2011@gmail.com>
2023-04-23 16:40:59 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- *main -Entry point
- *Description: prints all possible combinations of two two-digit numbers
- *
- *Return: Always 0
+ * main - Entry Point
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
  */
+
 int main(void)
 {
-	int i, n;
+	int firstDigit = 0, seconDigit;
 
-	for (i = 0; i < 100; i++)
+	while (firstDigit <= 99)
 	{
-		for (n = 0; n < 100; n++)
-	{
-	if (i < n)
-	{
-		putchar((i / 10) + 48);
-		putchar((i % 10) + 48);
-		putchar(' ');
-		putchar((n / 10) + 48);
-		putchar((n % 10) + 48);
-	if (i != 98 || n != 99)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	}
+		seconDigit = firstDigit;
+		while (seconDigit <= 99)
+		{
+			if (seconDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) + 48);
+				putchar((seconDigit % 10) + 48);
+
+				if (firstDigit != 98 || seconDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++seconDigit;
+		}
+		++firstDigit;
 	}
 	putchar('\n');
+
 	return (0);
 }
