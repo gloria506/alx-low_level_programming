ae98d2566a1f5b9f72b910b7429ce971f2b8aaf6
gloria506 <gloriaboateng2011@gmail.com>
2023-05-12 16:25:32 -0700

    error fixed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - Entry point
-  * Description - 'a function that prints a string, followed by a new line'
-  * @s: the char string type
+  * _print_rev_recursion - Entry point
+  * Description - 'a function that prints a string in reverse recursively'
+  * @s: string to be treated
   * Return: void
   */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	_puts_recursion(s + 1);
 }
